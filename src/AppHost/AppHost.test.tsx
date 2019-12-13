0b088e3711d0460be10b6e71e13dee0067ad64c4
Jeff <smn19c@my.fsu.edu>
2019-12-13 18:11:29 -0500

    forgot hidden files

--- src/AppHost/AppHost.test.tsx
+++ src/AppHost/AppHost.test.tsx
@@ -1,20 +1,18 @@
 import '@testing-library/jest-dom/extend-expect';
-import { getByTestId, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from '../Stores/StoreProvider';
 import AppHost from './AppHost';
 
 test('renders', () => {
-  const { getByText } = render(
+  const { getByTestId } = render(
     <BrowserRouter>
       <StoreProvider>
         <AppHost />
       </StoreProvider>
     </BrowserRouter>
   );
-  const appBarElement = getByTestId(document.body, 'appBarTest');
-  expect(appBarElement).toHaveTextContent(
-    'React-Functional-Mobx-Router-Material-Sample'
-  );
+  const appBarElement = getByTestId('appBarTest');
+  expect(appBarElement).toHaveTextContent('React-Functional-Mobx-Router-Material-Sample');
 });
